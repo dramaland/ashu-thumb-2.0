925962c982441679bb09feabe22c1a1258bc266e
qwwerty12390 <139214618+qwwerty12390@users.noreply.github.com>
2024-01-27 18:44:30 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,13 +7,13 @@ import re, os
 
 id_pattern = re.compile(r'^.\d+$') 
 
-API_ID = os.environ.get("API_ID", "23681812")
+API_ID = os.environ.get("API_ID", "27808193")
 
-API_HASH = os.environ.get("API_HASH", "3152d0cda0a21787db0c924f522374b7")
+API_HASH = os.environ.get("API_HASH", "c9964aa48c0fde0a3e7fee755a1b8a4e")
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN", "6858377496:AAEHVaWfhp-2LC11YEm4oSGes5Fw7iiNJJI") 
+BOT_TOKEN = os.environ.get("BOT_TOKEN", "6773697624:AAHkzuMir9droKuKg5kB4aBOvOZ9y98V2G4") 
 
-FORCE_SUB = os.environ.get("FORCE_SUB", "Dramafilez") 
+FORCE_SUB = os.environ.get("FORCE_SUB", "KOREAN_DRAMA_FILES_HINDI") 
 
              # Don't Remove Credit @VJ_Botz
              # Subscribe YouTube Channel For Amazing Bot @Tech_VJ
@@ -27,7 +27,7 @@ FLOOD = int(os.environ.get("FLOOD", "10"))
 
 START_PIC = os.environ.get("START_PIC", "https://te.legra.ph/file/119729ea3cdce4fefb6a1.jpg")
 
-ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '6710996831').split()]
+ADMIN = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '999739828').split()]
 
 PORT = os.environ.get("PORT", "8080")
 
